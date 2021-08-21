01595670429e3b851ed44f00422986fbb24daa9b
mhasancy <mhasancy@gmail.com>
2021-08-21 18:32:44 +0600

    fuction reduced

--- js/main.js
+++ js/main.js
@@ -1,104 +1,101 @@
-// variable for memory price, storage price , delivery charge and promo price
+// global variable
 const memoryPrice = document.getElementById("memory-price");
 const storagePrice = document.getElementById("storage-price");
 const deliveryCharge = document.getElementById("delivery-charge");
-const promoTotal = document.getElementById("promo-price");
+const totalPrice = document.getElementById("total-price");
 const promoButton = document.getElementById("promo-button");
+const promoTotal = document.getElementById("promo-price");
 
-// function for memory price update
-function memoryPriceUpdate(memorySize) {
-  const memoryPriceValue = document.getElementById("memory-" + memorySize);
-  const memoryPriceText = memoryPriceValue.value;
-  memoryPrice.innerText = parseInt(memoryPriceText);
-  total();
-  promoButtonActive();
-}
-// function for storage price update
-function storagePriceUpdate(storageSize) {
-  const storagePriceValue = document.getElementById("storage-" + storageSize);
-  const storagePriceText = storagePriceValue.value;
-  storagePrice.innerText = parseInt(storagePriceText);
-  total();
-  promoButtonActive();
-}
-// function for storage delivery charge update
+//function for memory, storage and delivery price update
 
-function deliveryChargeUpdate(deliveryMethod) {
-  const deliveryChargeValue = document.getElementById(
-    "delivery-" + deliveryMethod
-  );
-  const deliveryChargeText = deliveryChargeValue.value;
-  deliveryCharge.innerText = parseInt(deliveryChargeText);
-  total();
+function PriceUpdate(values) {
+  //condition for memory price update
+  if (values == "8gb" || values == "16gb") {
+    const memoryPriceText = document.getElementById("memory-" + values).value;
+    memoryPrice.innerText = parseInt(memoryPriceText);
+  }
+  //condition for storage price update
+  else if (values == "256gb" || values == "512gb" || values == "1tb") {
+    const storagePriceText = document.getElementById("storage-" + values).value;
+    storagePrice.innerText = parseInt(storagePriceText);
+  }
+  //condition for delivery charge update
+  else if (values == "free" || values == "paid") {
+    const deliveryChargeText = document.getElementById(
+      "delivery-" + values
+    ).value;
+    deliveryCharge.innerText = parseInt(deliveryChargeText);
+  }
+  //total price update and button activation
+  totalBalance();
   promoButtonActive();
 }
 
 // function for total price update and promo total update
 
-function total() {
-  const total = document.getElementById("total-price");
-  const totalMain =
+function totalBalance() {
+  //total price updated
+  const totalPriceUpdated =
     1299 +
-    Number(memoryPrice.innerText) +
-    Number(storagePrice.innerText) +
-    Number(deliveryCharge.innerText);
-  // console.log(deliveryPrice.innerText);
-  total.innerText = totalMain;
-  promoTotal.innerText = totalMain;
-  return totalMain;
+    parseInt(memoryPrice.innerText) +
+    parseInt(storagePrice.innerText) +
+    parseInt(deliveryCharge.innerText);
+  //total price and promo main price update
+  totalPrice.innerText = totalPriceUpdated;
+  promoTotal.innerText = totalPriceUpdated;
 }
+
 // function for promo total update with promo code
 
 function promoTotalUpdateWithCode() {
   const promoInput = document.getElementById("promo-input");
-  const promoValue = promoInput.value;
-  const promoTotalValue = parseInt(promoTotal.innerText);
-  if (promoValue == "stevekaku") {
-    const promoTotalCalc = promoTotalValue * 0.8;
-    const promoTotalUpdate = parseFloat(promoTotalCalc);
+  //promo code condition
+  if (promoInput.value == "stevekaku") {
+    //promo price calculation
+    const promoTotalUpdate = parseFloat(totalPrice.innerText) * 0.8;
+    //promo price update
     promoTotal.innerText = promoTotalUpdate;
     promoInput.value = "";
+    //promo button disabling
     promoButton.innerText = "Applied";
     promoButton.disabled = true;
   }
 }
-// function for promo button active
+
+// function for promo button enabling
 function promoButtonActive() {
   promoButton.disabled = false;
   promoButton.innerText = "Apply";
 }
-//event handler for memory option
 
+//event handler for memory option
 document.getElementById("memory-16gb").addEventListener("click", function () {
-  memoryPriceUpdate("16gb");
+  PriceUpdate("16gb");
 });
 document.getElementById("memory-8gb").addEventListener("click", function () {
-  memoryPriceUpdate("8gb");
+  PriceUpdate("8gb");
 });
 
 //event handler for storage option
-
 document.getElementById("storage-256gb").addEventListener("click", function () {
-  storagePriceUpdate("256gb");
+  PriceUpdate("256gb");
 });
 document.getElementById("storage-512gb").addEventListener("click", function () {
-  storagePriceUpdate("512gb");
+  PriceUpdate("512gb");
 });
 document.getElementById("storage-1tb").addEventListener("click", function () {
-  storagePriceUpdate("1tb");
+  PriceUpdate("1tb");
 });
 
 //event handler for delivery option
-
 document.getElementById("delivery-free").addEventListener("click", function () {
-  deliveryChargeUpdate("free");
+  PriceUpdate("free");
 });
 document.getElementById("delivery-paid").addEventListener("click", function () {
-  deliveryChargeUpdate("paid");
+  PriceUpdate("paid");
 });
 
 //event handler for promo code
-
 promoButton.addEventListener("click", function () {
   promoTotalUpdateWithCode();
 });
